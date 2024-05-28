e4e3c303f33e83a3523b1f95417875d69a90f63c
JoelCaza <65417728+JoelCaza@users.noreply.github.com>
2024-05-28 16:18:30 -0500

    Login Backend and Bdd

--- myproject/myapp/app_url.py
+++ myproject/myapp/app_url.py
@@ -3,12 +3,15 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+
     path('', views.model_list, name='model_list'),
+    path('admin', views.login_required, name='login'),
     path('create/', views.model_create, name='model_create'),
     path('<int:pk>/', views.model_detail, name='model_detail'),
     path('<int:pk>/update/', views.model_update, name='model_update'),
     path('<int:pk>/delete/', views.model_delete, name='model_delete'),
     path('<int:pk>/delete/confirm/', views.model_confirm_delete, name='model_confirm_delete'),
+
 ]
 
 
