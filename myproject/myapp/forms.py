90742667b1e2ff7b541676f5047ec1b5cf09f145
JoelCaza <65417728+JoelCaza@users.noreply.github.com>
2024-05-29 13:42:29 -0500

    Migrate Bdd todos los campos

--- myproject/myapp/forms.py
+++ myproject/myapp/forms.py
@@ -1,8 +1,23 @@
-
 from django import forms
 from .models import MyModel
 
 class MyModelForm(forms.ModelForm):
     class Meta:
         model = MyModel
-        fields = ['name', 'description']
+        fields = [
+            'codigo_bien', 
+            'codigo_anterior', 
+            'codigo_provisional', 
+            'codigo_nuevo', 
+            'nombre_bien', 
+            'serie', 
+            'modelo', 
+            'marca', 
+            'color', 
+            'material', 
+            'estado', 
+            'ubicacion', 
+            'cedula', 
+            'custodio_actual', 
+            'observacion'
+        ]
