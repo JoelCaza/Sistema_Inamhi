87ab552a06a20d6d2e3e8f4978b502fb4eb1fd15
JoelCaza <65417728+JoelCaza@users.noreply.github.com>
2024-05-29 16:30:34 -0500

    Cambios Full

--- myproject/myproject/settings.py
+++ myproject/myproject/settings.py
@@ -120,7 +120,7 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'myapp/static'),
+    os.path.join(BASE_DIR, 'myapp/static/styles'),
 ]
 
 # Default primary key field type
